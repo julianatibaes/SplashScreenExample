39b50bb6e6a4ea4f3eef404822d74982aa5a5d4f
julianatibaes <tibaes.juliana@gmail.com>
2017-03-13 00:00:24 -0300

    Initial commit
    Projeto

---
+++ SplashScreenExample/app/src/main/java/com/tibaes/splashscreenexample/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.tibaes.splashscreenexample;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    // Timer da splash screen
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Exibindo splash com um timer.
+             */
+            @Override
+            public void run() {
+                // Esse método será executado sempre que o timer acabar
+                // E inicia a activity principal
+
+                Intent intent = new Intent(SplashScreenActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+
+
+                // Fecha esta activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
